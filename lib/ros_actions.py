2341bdefea00b76f02deb108eb46ce63a47371ca
“Milad244” <145618774+Milad244@users.noreply.github.com>
2026-07-11 20:30:28 -0700

    Remove hard coded session name

--- lib/ros_actions.py
+++ lib/ros_actions.py
@@ -1,13 +1,13 @@
-from config import RUN_SIM, SOURCE_ENV_CMD, RUN_TELEOP
+from config import RUN_SIM, SOURCE_ENV_CMD, RUN_TELEOP, SESSION_NAME
 from lib.tmux_utils import send_keys
 
 def source_window(window_name: str) -> None:
-    send_keys(f"f1tenth:{window_name}", SOURCE_ENV_CMD)
+    send_keys(f"{SESSION_NAME}:{window_name}", SOURCE_ENV_CMD)
 
 def launch_sim(window_name: str) -> None:
     source_window(window_name)
-    send_keys(f"f1tenth:{window_name}", RUN_SIM)
+    send_keys(f"{SESSION_NAME}:{window_name}", RUN_SIM)
 
 def launch_teleop(window_name: str) -> None:
     source_window(window_name)
-    send_keys(f"f1tenth:{window_name}", RUN_TELEOP)
+    send_keys(f"{SESSION_NAME}:{window_name}", RUN_TELEOP)
